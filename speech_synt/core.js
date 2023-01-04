bb796a0d343353f4b6fb84df9d7a42873f554690
StasKlymenko <klymenko.stas@gmail.com>
2023-01-04 21:59:57 +0200

    Add voices and buttons

--- speech_synt/core.js
+++ speech_synt/core.js
@@ -1,20 +1,36 @@
-const synt = window.speechSynthesis
-synt.addEventListener("voiceschanged", () => addVoices())
-
+let VOICES = []
+let PAUSE = false
+const synth = window.speechSynthesis
 const inputText = document.querySelector("textarea")
-addVoices()
+speechSynthesis.addEventListener("voiceschanged", () => addVoices())
 
-document.querySelector("button").addEventListener("click", () => {
+
+document.querySelector(".start").addEventListener("click", () => {
+    synth.cancel()
     const utterThis = new SpeechSynthesisUtterance(inputText.value)
-    synt.speak(utterThis)
+    utterThis.voice = VOICES[document.querySelector(".voices").value]
+    synth.speak(utterThis)
 })
 
+document.querySelector(".pause").addEventListener("click", () => {
+    console.log(PAUSE)
+    PAUSE = !PAUSE
+    if(PAUSE) {
+        synth.pause()
+    } else {
+        synth.resume()
+    }
+})
+
+document.querySelector(".stop").addEventListener("click", () => {
+    synth.cancel()
+})
 
 async function addVoices() {
-    const voices = synt.getVoices()
-    voices.forEach(voice => {
+    VOICES = synth.getVoices()
+    VOICES.forEach((voice, index) => {
         const voiceElement = document.createElement("option")
-        voiceElement.value = voice.voiceURI
+        voiceElement.value = index
         voiceElement.innerText = voice.name
         console.log(voiceElement)
         document.querySelector(".voices").appendChild(voiceElement)
