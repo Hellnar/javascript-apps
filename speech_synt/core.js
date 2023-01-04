dfc5a31a0d4ef39a7fc2d715de3f3f00bbaf4625
StasKlymenko <klymenko.stas@gmail.com>
2023-01-04 21:30:37 +0200

    Add select voices

--- speech_synt/core.js
+++ speech_synt/core.js
@@ -1,8 +1,22 @@
 const synt = window.speechSynthesis
+synt.addEventListener("voiceschanged", () => addVoices())
 
 const inputText = document.querySelector("textarea")
+addVoices()
 
 document.querySelector("button").addEventListener("click", () => {
     const utterThis = new SpeechSynthesisUtterance(inputText.value)
     synt.speak(utterThis)
 })
+
+
+async function addVoices() {
+    const voices = synt.getVoices()
+    voices.forEach(voice => {
+        const voiceElement = document.createElement("option")
+        voiceElement.value = voice.voiceURI
+        voiceElement.innerText = voice.name
+        console.log(voiceElement)
+        document.querySelector(".voices").appendChild(voiceElement)
+    })
+}
