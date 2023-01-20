5f57192c35788add4494ec7ba6c1c0130a0baa15
StasKlymenko <klymenko.stas@gmail.com>
2023-01-20 23:17:52 +0200

    Add random script for pass generator

--- projects/beginner/password_generator/scripts.js
+++ projects/beginner/password_generator/scripts.js
@@ -1,4 +1,9 @@
 document.querySelector("button").addEventListener("click", () => {
-    const pass = "Generated pass"
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*+-=?"
+    let pass = ""
+    for(let length = 14; length > 0; length--) {
+        const randomNumber = Math.floor(Math.random() * chars.length)
+        pass += chars.charAt(randomNumber)
+    }
     document.querySelector("input").value = pass
 })
